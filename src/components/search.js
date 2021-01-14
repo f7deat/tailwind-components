9dda9ca45c5090bd53385d07d02561fd4774b750
f7deat <tan.dct9x@gmail.com>
2021-01-14 17:42:53 +0700

    add react router dom

--- src/components/search.js
+++ src/components/search.js
@@ -5,17 +5,21 @@ export default function Search() {
             <div className="w-full max-w-xs mr-2 navbar-search">
                 <div className="relative">
                     <input type="text" name="search" placeholder="Search..." className="border pl-8 pr-5 appearance-none h-10 w-full rounded-full text-sm focus:outline-none" autoComplete="off" />
-                    <button type="submit" className="absolute top-0 left-0 py-3 px-3 text-gray-400">
-                        <ion-icon name="search"></ion-icon>
+                    <button type="submit" className="absolute top-0 left-0 p-3 text-gray-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-4 w-4">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                        </svg>
                     </button>
                 </div>
             </div>
-            <textarea disabled={true} className="mt-4 bg-gray-900 text-gray-200 p-4 rounded w-full h-48 text-sm"
+            <textarea disabled={true} className="mt-4 bg-gray-900 text-gray-200 p-4 rounded w-full h-60 text-sm"
                 value={`<div class="w-full max-w-xs mr-2 navbar-search">
     <div class="relative">
         <input type="text" name="search" placeholder="Search..." class="border pl-8 pr-5 appearance-none h-10 w-full rounded-full text-sm focus:outline-none" autoComplete="off" />
-        <button type="submit" class="absolute top-0 left-0 py-3 px-3 text-gray-400">
-            <ion-icon name="search"></ion-icon>
+        <button type="submit" className="absolute top-0 left-0 p-3 text-gray-400">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-4 w-4">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
         </button>
     </div>
 </div>`} />
--- src/layout/sidebar.js
+++ src/layout/sidebar.js
@@ -3,6 +3,7 @@ function Sidebar() {
         <div className="ml-10">
             <Item name="Home" icon="home" />
             <Item name="Input" icon="pencil" hasChild={true} />
+            <Item name="Card" icon="home" />
         </div>
     )
 }
@@ -12,7 +13,9 @@ function Item(props) {
         <div>
             <div className="cursor-pointer font-medium p-3 flex items-center hover:text-red-500 d-flex">
                 <div className="flex-grow flex items-center">
-                    <ion-icon name={props.icon}></ion-icon>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h5 w-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 4h13M3 8h9m-9 4h9m5-4v12m0 0l-4-4m4 4l4-4" />
+                    </svg>
                     <div className="ml-2">{props.name}</div>
                 </div>
                 {
